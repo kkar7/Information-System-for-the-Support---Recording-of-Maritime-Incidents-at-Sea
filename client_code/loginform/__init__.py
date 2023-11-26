1485f7a63041ced637d7fb7290c17323ef0568ee
konstantinoschios@hotmail.com <konstantinoschios@hotmail.com>
2023-11-26 21:20:04 +0000

    Created loginform; Edited settings; Deleted Form1

--- client_code/loginform/__init__.py
+++ client_code/loginform/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import loginformTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class loginform(loginformTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
