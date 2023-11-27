417fdc6170e7e85bc9172bf7e672b531d1d30587
konstantinoschios@hotmail.com <konstantinoschios@hotmail.com>
2023-11-27 20:50:30 +0000

    Created ServerModule1 and Form1.ItemTemplate1; Edited loginform and Form1

--- client_code/Form1/ItemTemplate1/__init__.py
+++ client_code/Form1/ItemTemplate1/__init__.py
@@ -1,14 +1,10 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import ItemTemplate1Template
 from anvil import *
 import anvil.server
 
-class Form1(Form1Template):
+class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def text_box_1_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    pass
--- client_code/loginform/__init__.py
+++ client_code/loginform/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import loginformTemplate
 from anvil import *
+import anvil.server
 
 class loginform(loginformTemplate):
   def __init__(self, **properties):
