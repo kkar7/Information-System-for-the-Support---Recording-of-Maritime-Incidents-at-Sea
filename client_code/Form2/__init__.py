40dde83927b5ad6c51f0d31a75f69ddc28f2ae91
konstantinoschios@hotmail.com <konstantinoschios@hotmail.com>
2024-02-11 17:22:44 +0000

    Edited settings and Form2; Deleted Form1_copy

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -1,10 +1,10 @@
 from ._anvil_designer import Form2Template
 from anvil import *
+import anvil.server
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
-import anvil.server
 
 class Form2(Form2Template):
   def __init__(self, **properties):
@@ -12,3 +12,15 @@ class Form2(Form2Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+
+  def map_1_bounds_changed(self, **event_args):
+    """This method is called when the viewport bounds have changed."""
+    marker = GoogleMap.Marker(
+    animation=GoogleMap.Animation.DROP,
+    position=GoogleMap.LatLng(52.2053, 0.1218)
+    )
+    map.add_component(marker)
+    pass
+
+
