0303a2a5eb1dfea944028036b5f1f8417ba5fa85
konstantinoschios@hotmail.com <konstantinoschios@hotmail.com>
2024-02-22 12:41:59 +0000

    Created map and Start; Deleted Form3

--- client_code/map/__init__.py
+++ client_code/map/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form3Template
+from ._anvil_designer import mapTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,7 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form3(Form3Template):
+class map(mapTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
